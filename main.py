68a4703590b3a2a04268076cbc5fc461137c657f
Shapovalov Mihail <mihailsapovalov05@gmail.com>
2023-02-19 17:51:37 +0300

    Add origin

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://192.168.0.210:3000"
+    "http://192.168.0.210:3000",
+    "http://172.20.10.2:3000"
 ]
 
 app.add_middleware(
